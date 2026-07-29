91abcdc75d785704b3ee310bc80ae016d5ab98be
Soham Aute <sohamaute450@gmail.com>
2026-07-29 10:16:06 +0530

    Create instancemethod.py

---
+++ instancemethod.py
@@ -0,0 +1,11 @@
+class student:
+
+    def display(self):
+        print("welcome to python programming")
+
+        #creating object
+        s1=student()
+        
+        #calling instance method
+        s1.dispaly()
+        
